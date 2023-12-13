878ed4767cb440e1bd8da9d76a16660006d2da81
Dariusz Szyperek <dariusz_szyperek@epam.com>
2023-12-13 14:15:05 +0100

    Decorator

--- src/adapter/SquarePegAdapter.java
+++ src/adapter/SquarePegAdapter.java
@@ -4,7 +4,7 @@ package adapter;
  * Adapter allows fitting square pegs into round holes.
  */
 public class SquarePegAdapter extends RoundPeg {
-    private SquarePeg peg;
+    private final SquarePeg peg;
 
     public SquarePegAdapter(SquarePeg peg) {
         this.peg = peg;
@@ -14,7 +14,8 @@ public class SquarePegAdapter extends RoundPeg {
     public double getRadius() {
         double result;
         // Calculate a minimum circle radius, which can fit this peg.
-        result = (Math.sqrt(Math.pow((peg.getWidth() / 2), 2) * 2));
+        result = peg.getWidth() / Math.sqrt(2);
+        System.out.println(result);
         return result;
     }
 }
